7f22ebca54a9a2e9c99faecd3cd0b5d945876195
Andrii Kostenko <andrii.kostenko@tallium.com>
2025-05-09 18:12:34 +0300

    added user fetching to Profile

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-const ProfileScreen: React.FC = () => {
+import { useQuery } from '@tanstack/react-query';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
+import { useAuth } from '../context/AuthContext';
+import { userService } from '../services';
+import { NavigationRoutes } from "../enums/navigation.enum";
+import { RootStackParamList } from "../navigation/AppNavigator";
+import { User } from '../types';
+import { CustomButton } from '../ui';
+
+const Profile: React.FC = () => {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+    const { user, logout } = useAuth();
+
+    const queryResult = useQuery<User, Error>({
+        queryKey: ['user'],
+        queryFn: () => userService.fetchUser(user!.accessToken),
+        enabled: !!user?.accessToken,
+    });
+
+    const { data: userData, error, isLoading } = queryResult;
+
+    if (!user) {
+        navigation.navigate(NavigationRoutes.HOME);
+        return null;
+    }
+
+    const handleLogout = () => {
+        logout();
+        navigation.navigate(NavigationRoutes.HOME);
+    };
+
+    if (isLoading) {
+        return <Text>Loading user data...</Text>;
+    }
+
+    if (error) {
+        return <Text>Error: {error.message}</Text>;
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Profile Screen</Text>
+            <Text style={styles.title}>Profile</Text>
+            <Text>Username: {userData?.username}</Text>
+            <Text>Email: {userData?.email}</Text>
+            <Text>First Name: {userData?.firstName}</Text>
+            <Text>Last Name: {userData?.lastName}</Text>
+            <CustomButton onPress={handleLogout} label='Logout' />
         </View>
     );
 };
@@ -13,12 +56,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        padding: 20,
     },
     title: {
         fontSize: 24,
-        marginBottom: 16,
+        marginBottom: 20,
+        textAlign: 'center',
     },
 });
 
-export default ProfileScreen;
+export default Profile;
